8babf55cebb74d8c7205d907ab0e8abf3007d485
lazy_slipper <100974968+AnnaLuna111@users.noreply.github.com>
2023-05-25 16:02:05 +0300

    Add filter and placeholder

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './view/add-view.js';
 import './view/filter-view.js';
 import './view/sort-view.js';
 import './view/list-view.js';
+import './view/placeholder-view.js';
 
 import AppModel from './models/app-model.js';
 import BriefPresenter from './presenter/brief-presenter.js';
@@ -10,6 +11,7 @@ import AddPresenter from './presenter/add-presenter.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import SortPresenter from './presenter/sort-presenter.js';
 import ListPresenter from './presenter/list-presenter.js';
+import PlaceholderPresenter from './presenter/placeholder-presenter.js';
 
 const appModel = new AppModel();
 
@@ -18,3 +20,4 @@ new AddPresenter(document.querySelector('add-view'));
 new FilterPresenter(document.querySelector('filter-view'));
 new SortPresenter(document.querySelector('sort-view'));
 new ListPresenter(document.querySelector('list-view'), appModel);
+new PlaceholderPresenter(document.querySelector('placeholder-view'), appModel);
