61f530495301097cbbbe15d03b800826c20b77ec
lazy_slipper <100974968+AnnaLuna111@users.noreply.github.com>
2023-06-13 21:22:44 +0300

    Add api data

--- src/types.js
+++ src/types.js
@@ -89,7 +89,7 @@
 /**
  * @typedef PlaceholderViewState
  * @prop {string} text
- * @prop {boolean} isHidden
+ * @prop {boolean} [isHidden]
  */
 
 /**
@@ -97,6 +97,18 @@
  * @prop {string} [edit]
  * @prop {FilterType} [filter]
  * @prop {SortType} [sort]
+ */
+
+/**
+ * @typedef {import('./services/api-service.js').default} ApiService
+ * @typedef {import('./services/service.js').default} Service
+ */
+
+/**
+ * @typedef ServiceOptions
+ * @prop {string} baseUrl
+ * @prop {number} minResponseTime
+ * @prop {string} authorization
  */
 
 /**
